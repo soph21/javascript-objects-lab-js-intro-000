5e21ab110cb6d41a36877819696d9dfa6b79318f
Sophie Shin <sophie@alloy.co>
2021-03-11 19:46:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,3 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
   object[key] = value
   return object
 }
-var object = {key: 'value'}
-var newObject = Object.assign({}, object)
-function deleteFromObjectByKey(object, key) {
-  delete newObject[key]
-  return Object.assign({}, object, {[key]: value})
-}
